f1908af9ceea30f90ca82c704136fc02a578f109
rivahq-ryn <ryan@rivahq.com>
2020-02-18 17:00:37 -0800

    Add functionality to pwd commands (with flags, -L & -P). -L (default if no flag) prints current working directory with symbolic link. -P prints current directory without symbolic link.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Display from './components/Display';
 import Prompt from './components/Prompt';
 import './App.css';
+const fs = window.require('fs');
 const { app } = window.require('electron').remote;
 const home: string = app.getPath('home');
 
@@ -12,12 +13,46 @@ export interface HistoryObject {
 
 function App() {
   const [history, setHistory] = useState<HistoryObject[]>([]);
-  const [path, setPath] = useState<string>(home);
+  const [path, setPath] = useState<string>(
+    home + '/documents/desktop_application/terminal/symlink'
+  );
+
+  const commands = (text: string) => {
+    // TODO: split commands and evaluate flags after (pwd -> -L/-P)
+    switch (text) {
+      case 'hi':
+      case 'hello':
+        return 'hello to you too! 🐢';
+      case 'pwd':
+      case 'pwd -L':
+        return path;
+      case 'pwd -P':
+        let resolvedPath;
+        try {
+          const resolved = fs.readlinkSync(path);
+          const tmp = path.split('/');
+          tmp.pop();
+          tmp.push(resolved);
+          resolvedPath = tmp.join('/');
+        } catch (e) {
+          console.log(e);
+          resolvedPath = path;
+        }
+        return resolvedPath;
+      default:
+        return `tortoise: command not found: ${text}`;
+    }
+  };
 
   return (
     <div className="App">
-      <Display History={history} Path={home} />
-      <Prompt History={history} SetHistory={setHistory} Path={home} />
+      <Display history={history} path={path} />
+      <Prompt
+        history={history}
+        setHistory={setHistory}
+        path={path}
+        commands={commands}
+      />
     </div>
   );
 }
